98cf6671d94c4bcac842ea02a6a8f619c6a0f1f9
Necmi Atılbaz <necmiatilbazz@gmail.com>
2022-10-20 16:03:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tmrYaris = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AtYarisi.Properties.Resources.horse1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AtYarisi.Properties.Resources.horse3;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::AtYarisi.Properties.Resources.horse2;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 184);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(12, 318);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(79, 86);
+            this.btnBasla.TabIndex = 3;
+            this.btnBasla.Text = "Yarışı Başlat";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Location = new System.Drawing.Point(1409, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 500);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // tmrYaris
+            // 
+            this.tmrYaris.Tick += new System.EventHandler(this.tmrYaris_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1444, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer tmrYaris;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            tmrYaris.Start();
+        }
+
+        private void tmrYaris_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left+=rnd.Next(1,50);
+            pictureBox2.Left+=rnd.Next(1,50);
+            pictureBox3.Left+=rnd.Next(1,50);
+
+            if (pictureBox1.Right>=label1.Left)
+            {
+                tmrYaris.Stop();
+                DialogResult dr = MessageBox.Show("1.At yarışı kazandı.Tekrar oynamak ister misiniz?", "At Yarışı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    pictureBox1.Left=pictureBox2.Left=pictureBox3.Left=0;
+                    tmrYaris.Start();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            else if (pictureBox2.Right>=label1.Left)
+            {
+                tmrYaris.Stop();
+                DialogResult dr = MessageBox.Show("2.At yarışı kazandı.Tekrar oynamak ister misiniz?", "At Yarışı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    pictureBox1.Left = pictureBox2.Left = pictureBox3.Left = 0;
+                    tmrYaris.Start();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            else if (pictureBox3.Right>=label1.Left)
+            {
+
+                tmrYaris.Stop();
+                DialogResult dr = MessageBox.Show("3.At yarışı kazandı.Tekrar oynamak ister misiniz?", "At Yarışı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dr == DialogResult.Yes)
+                {
+                    pictureBox1.Left = pictureBox2.Left = pictureBox3.Left = 0;
+                    tmrYaris.Start();
+                }
+                else
+                {
+                    Close();
+                }
+
+            }
+        }
+        //Timer başladığı zaman atlar rastgele bir sayıya göre hareket edecek.
+        //picturebox ın sağının konumu label ın solunun konumuna değdiği zaman oyun bitecek
+        //?? random sayi ile picturebox nasıl taşınır ? (+=)
+        // oyun bitişinde 1.,2,3. at kazandı yazısı ekrana gösterilecek (hangisi kazanırsa.)
+    }
+}
